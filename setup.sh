76913e2822714f06d2f4094a3a673d6a3fae4845
AshtonautEdu <jzqq6560@leeds.ac.uk>
2025-07-29 18:51:42 +0100

    Modify paths

--- setup.sh
+++ setup.sh
@@ -1,9 +1,20 @@
-#!/bin/sh
+#!/usr/bin/env bash
 # PTS-Pack setup script
 #
 # A script designed to setup the environment for PTS-Pack use
 
-source ./spack/share/spack/setup-env.sh
+# Dir setup
+if [ -n "$SLURM_JOB_ID" ]; then
+	SCRIPT_PATH=$(dirname $(scontrol show job $SLURM_JOB_ID | awk -F= '/Command=/{print $2}'))
+else
+	SCRIPT_PATH=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+fi
+
+
+### MAIN ###
+
+
+source ${SCRIPT_PATH}/spack/share/spack/setup-env.sh
 spack load php-pts
-alias pts=./phoronix-test-suite/phoronix-test-suite
+alias pts=${SCRIPT_PATH}/phoronix-test-suite/phoronix-test-suite
 export TMPDIR=./tmp
